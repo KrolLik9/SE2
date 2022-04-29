a07d16b7adb5af669708f80e40c54c340d820155
Elena <krollik2014@mail.ru>
2022-04-29 15:59:28 +0500

    add diff

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
- int x, y, c;
+ int x, y, c, d;
  cout << "Enter x:"; cin >> x;
  cout << "Enter y:"; cin >> y;
  c = x+y;
  cout << "count = ";cout << c;
+ d = x - y;
+ cout << "diff = ";cout << d;
  return 0;
 }
